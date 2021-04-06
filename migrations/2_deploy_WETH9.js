e34fe1e3bc8b6090140e23f2359d9fa1f6e9a020
daoswap001 <daoswap@daoswap.global>
2021-04-06 16:26:31 +0800

    replace WETH to WHT

--- migrations/2_deploy_WETH9.js
+++
@@ -1,5 +0,0 @@
-const WETH9 = artifacts.require("WETH9"); 
-
-module.exports = function(deployer) {
-    deployer.deploy(WETH9);
-};
--- migrations/5_deploy_UniswapV2Router02.js
+++ migrations/5_deploy_UniswapV2Router02.js
@@ -3,6 +3,6 @@ const UniswapV2Router02 = artifacts.require("UniswapV2Router02");
 module.exports = function(deployer) {
     deployer.deploy(UniswapV2Router02,
         '0x7908e858E2769923bB9eb75221765656D8B8E9AC', // 工厂合约地址
-        '0xc778417E063141139Fce010982780140Aa0cD5Ab' // WETH 地址
+        '0xc778417E063141139Fce010982780140Aa0cD5Ab' // WHT 地址
     );
 };
